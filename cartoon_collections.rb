0e0edcb0c628439c099f2495a0afdf3788ebcfa3
christian iverson <chris.w.iverson@gmail.com>
2020-12-10 21:14:29 -0800

    commit tree 545a576fd1a06fc0e3cd4cdcb40b51d514d2161d

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) {
+    |dwarves, index| puts "#{index}. #{dwarves}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(team)
+  team.map {
+    |element| "#{element.capitalize}!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find {
+    |i| cheese_types.include?(i)
+  }
+  
 end
